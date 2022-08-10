28d98dae708564066c6a4ad8467ede3a3cf38b92
notmatical <defyglide@gmail.com>
2022-08-10 19:17:13 -0400

    Update createApolloClient.ts

--- src/config/createApolloClient.ts
+++ src/config/createApolloClient.ts
@@ -2,18 +2,10 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { offsetLimitPagination, relayStylePagination } from '@apollo/client/utilities';
 
 const client = new ApolloClient({
-    cache: new InMemoryCache({
-        typePolicies: {
-            Query: {
-                fields: {
-                    leaderboard: relayStylePagination()
-                }
-            }
-        }
-    }),
+    cache: new InMemoryCache(),
     credentials: 'include',
     // uri: 'http://localhost:8080/graphql'
-    link: createHttpLink({ uri: '/graphql' })
+    link: createHttpLink({ uri: '/api/graphql' })
 });
 
 export default client;
